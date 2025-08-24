81e6564ad00feccd31f1c3b7f3b6e31d94dd3f74
forelius <140267826+Forelius@users.noreply.github.com>
2025-08-24 12:27:46 -0700

    Fixed issue where game packs weren't being set in game settings if null. Updated class items and saving throws.

--- scripts/fadeWhiteBox.mjs
+++ scripts/fadeWhiteBox.mjs
@@ -10,7 +10,7 @@ Hooks.once('beforeFadeInit', async function (fadeRegistry) {
 Hooks.once('afterFadeInit', async function (fadeRegistry) {
    console.debug('FWB: afterFadeInit hook called.');
 
-   Object.assign(game.settings.settings.get("fantastic-depths.abilityScoreModSystem").choices, {
+   Object.assign(game.settings.get("fantastic-depths.abilityScoreModSystem").choices, {
       simple: game.i18n.localize("SETTINGS.abilityScoreModSystem.choices.simple")
    });
    Object.assign(CONFIG.FADE.abilityScoreModSystem, FADEWB.abilityScoreModSystem);
@@ -20,8 +20,20 @@ Hooks.once('afterFadeInit', async function (fadeRegistry) {
    fadeRegistry.registerSystem('moraleCheck', new moraleCheck(), moraleCheck);
 });
 
-Hooks.once('beforeFadeReady', async function (fadeRegistry) {
+Hooks.once('beforeFadeReady', async (fadeRegistry) => {
    console.debug('FWB: beforeFadeReady hook called.');
+   if (!game.settings.get("fantastic-depths", "actorPack")) {
+      console.log("No actors pack specified, assigning white box.")
+      await game.settings.set("fantastic-depths", "actorPack", "fade-white-box-fmag.white-box-actors");
+   }
+   if (!game.settings.get("fantastic-depths", "itemPack")) {
+      console.log("No items pack specified, assigning white box.")
+      await game.settings.set("fantastic-depths", "itemPack", "fade-white-box-fmag.white-box-items");
+   }
+   if (!game.settings.get("fantastic-depths", "rollTablePack")) {
+      console.log("No roll-tables pack specified, assigning white box.")
+      await game.settings.set("fantastic-depths", "rollTablePack", "fade-white-box-fmag.white-box-roll-tables");
+   }
 });
 
 Hooks.once('afterFadeReady', async function (fadeRegistry) {
